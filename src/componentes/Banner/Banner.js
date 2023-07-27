ec93c51e4a85221e8c0f87ab445d4b3168ba8eb6
Evelinzz <evelingleal1@gmail.com>
2023-07-26 23:03:09 -0300

    adicionando imagens, css e excluindo a função e arquivos padão do react

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+    //JSX, permite uma escrita mais proxima ao HTML para manipular elementos DOM
+    <header className="banner">
+        <img src="/imagens/banner.png" alt="O banner principal da pagina do Organo"/>
+    </header>    
+    )
+}
+
+export default Banner
