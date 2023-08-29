cc4d30a6f27b55dd0e0427215109e81629c64674
Evelinzz <evelingleal1@gmail.com>
2023-08-28 22:59:37 -0300

    css details

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 
   const [colaboradores, setColaboradores] = useState([])
   const aoNovoColaboradorAdicionado = (colaborador) => {
+    setColaboradores([...colaboradores, colaborador])
     console.log(colaborador)
+  
   }
 
   return (
     <div className="App">
       <Banner/>  
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+      <Time nome="Programação"/>
+      <Time nome="Frontend"/>
+      <Time nome="DataScience"/>
+      <Time nome="DevOps"/>
+      <Time nome="UX e Design"/>
+      <Time nome="Mobile"/>
+      <Time nome="Inovação"/>
     </div>
   );
 }
